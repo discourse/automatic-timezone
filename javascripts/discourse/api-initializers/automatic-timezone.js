30966949a0698922c729bd2b9ac80fbc1e1a63fa
Martin Brennan <martin@discourse.org>
2025-01-14 15:17:29 +1000

    DEV: Use new core API for beforeSave for user profile

--- javascripts/discourse/api-initializers/automatic-timezone.js
+++ javascripts/discourse/api-initializers/automatic-timezone.js
@@ -6,34 +6,24 @@ export default apiInitializer("1.14.0", (api) => {
     return;
   }
 
-  const browserTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-  const lastDetectedTimezone =
-    currentUser.custom_fields?.last_detected_timezone;
+  const browserTimezone = moment.tz.guess();
+  const lastDetectedTimezone = currentUser.timezone;
   const wasManuallySet =
     currentUser.custom_fields?.manually_set_timezone === "true";
 
   if (!wasManuallySet || browserTimezone !== lastDetectedTimezone) {
     currentUser.set("timezone", browserTimezone);
-    currentUser.set("custom_fields.last_detected_timezone", browserTimezone);
     currentUser.set("custom_fields.manually_set_timezone", "false");
-    currentUser.save();
+    try {
+      currentUser.save();
+    } catch {
+      currentUser.set("timezone", lastDetectedTimezone);
+    }
   }
 
-  api.modifyClass("controller:preferences/interface", {
-    pluginId: "auto-timezone",
-
-    actions: {
-      save() {
-        if (this.model.timezone !== browserTimezone) {
-          this.model.set("custom_fields.manually_set_timezone", "true");
-          this.model.set(
-            "custom_fields.last_detected_timezone",
-            browserTimezone
-          );
-        }
-        return this._super(...arguments);
-      },
-    },
+  api.registerUserProfileBeforeSaveCallback((userModel) => {
+    if (userModel.timezone !== browserTimezone) {
+      userModel.set("custom_fields.manually_set_timezone", "true");
+    }
   });
 });
